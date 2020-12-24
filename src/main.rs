fbe8d1c4fdd8c41ae986e54e956d8f89ad5720f8
0xflotus <0xflotus@gmail.com>
2020-12-24 06:19:42 +0100

    add: converting to hex

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,45 @@ fn main() {
                 .required(false)
                 .index(1),
         )
+        .arg(
+            Arg::new("month")
+                .about("Sets the month")
+                .required(false)
+                .index(2),
+        )
+        .arg(
+            Arg::new("day")
+                .about("Sets the day")
+                .required(false)
+                .index(3),
+        )
         .get_matches();
 
     if let Some(year) = matches.value_of("year") {
-        println!("Provided year: {}", year);
+        if let Some(month) = matches.value_of("month") {
+            if let Some(day) = matches.value_of("day") {
+                println!(
+                    "{}-{}-{}",
+                    format!("{:#06x}", year.parse::<i32>().unwrap()),
+                    format!("{:02x}", month.parse::<i32>().unwrap()),
+                    format!("{:02x}", day.parse::<i32>().unwrap())
+                );
+            } else {
+                println!("Please provide a day.");
+            }
+        } else {
+            println!("Please provide a month.");
+        }
     } else {
         let now = Utc::now();
         let (_is_common_era, year) = now.year_ce();
-        println!("Provided year: {}", year)
+        let month = now.month();
+        let day = now.day();
+        println!(
+            "{}-{}-{}",
+            format!("{:#06x}", year),
+            format!("{:02x}", month),
+            format!("{:02x}", day)
+        );
     }
 }
