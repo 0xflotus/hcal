b87b0d79ce4d6875f5e52b6181a4b24715706841
0xflotus <0xflotus@gmail.com>
2020-12-24 05:58:13 +0100

    add: datetime and arg parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use chrono::{Datelike, Utc};
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("hcal")
+        .version("0.1.0")
+        .author("0xflotus")
+        .about("A hexadecimal calendar")
+        .arg(
+            Arg::new("year")
+                .about("Sets the year")
+                .required(false)
+                .index(1),
+        )
+        .get_matches();
+
+    if let Some(year) = matches.value_of("year") {
+        println!("Provided year: {}", year);
+    } else {
+        let now = Utc::now();
+        let (_is_common_era, year) = now.year_ce();
+        println!("Provided year: {}", year)
+    }
 }
