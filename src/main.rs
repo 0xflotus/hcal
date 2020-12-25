97cf293c9ebedccbf5962738fe03e5c48499d437
0xflotus <0xflotus@gmail.com>
2020-12-25 20:21:32 +0100

    v0.1.5

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::vec::Vec;
 
 fn main() {
     let matches = App::new("hcal")
-        .version("0.1.4")
+        .version("0.1.5")
         .author("0xflotus")
         .about("A hexadecimal calendar")
         .arg(
