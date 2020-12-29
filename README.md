092cb599244815ffd4a5b6a940d9f4c765cdd291
0xflotus <0xflotus@gmail.com>
2020-12-29 15:16:32 +0100

    support for balanced ternary representation

--- README.md
+++ README.md
@@ -8,8 +8,38 @@ A hexadecimal calendar for terminal
 
 `cargo install hcal`
 
+## Examples
+
+![hex](./docs/hex.png)
+
+![balanced ternary](./docs/bal_ternary.png)
+
 ## Usage
 
-`hcal --help`
+```console
+> hcal --help
+A hexadecimal calendar
+
+USAGE:
+    hcal [FLAGS] [OPTIONS] [ARGS]
+
+ARGS:
+    <year>     Sets the year
+    <month>    Sets the month
+    <day>      Sets the day
+
+FLAGS:
+    -b, --balanced-ternary    Use balanced ternary representation
+    -d, --disable             Disable day marker
+    -A, --disbale-all         Disable all font effects
+    -E, --effect              Enable title font effects
+    -h, --help                Prints help information
+    -W, --no-weekend          Disable weekend marker
+    -V, --version             Prints version information
+
+OPTIONS:
+    -e, --easter <year>       Prints the Hex Date of easter.
+    -T, --transform <date>    Prints the Hex Date of <date>. Needs format of dd-mm-yyyy.
+```
 
 [![asciicast](https://asciinema.org/a/381223.svg)](https://asciinema.org/a/381223)
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use regex::Regex;
 use std::process;
 use std::vec::Vec;
 
+use cbb::util::cbb::int_to_bal_ternary;
+
 mod helpers;
 use helpers::{cal, hex};
 
@@ -28,6 +30,12 @@ fn main() {
                 .takes_value(true)
                 .value_name("date")
                 .about("Prints the Hex Date of <date>. Needs format of dd-mm-yyyy."),
+        )
+        .arg(
+            Arg::new("balanced-ternary")
+                .short('b')
+                .long("balanced-ternary")
+                .about("Use balanced ternary representation"),
         )
         .arg(
             Arg::new("disable")
@@ -84,11 +92,9 @@ fn main() {
         });
         println!(
             "{}",
-            format!(
-                "{:#06x}-{:#04x}-{:#04x}",
-                easter_date.year(),
-                easter_date.month(),
-                easter_date.day()
+            format_date(
+                (easter_date.year(), easter_date.month(), easter_date.day()),
+                matches.is_present("balanced-ternary")
             )
         );
         process::exit(0_i32);
@@ -101,23 +107,21 @@ fn main() {
         });
         if re.is_match(date) {
             let splitted_date: Vec<&str> = date.split('-').collect();
+            let year = splitted_date[2].parse::<i32>().unwrap_or_else(|_| {
+                println!("Error while parsing year");
+                process::exit(1_i32);
+            });
+            let month = splitted_date[1].parse::<u32>().unwrap_or_else(|_| {
+                println!("Error while parsing month");
+                process::exit(1_i32);
+            });
+            let day = splitted_date[0].parse::<u32>().unwrap_or_else(|_| {
+                println!("Error while parsing day");
+                process::exit(1_i32);
+            });
             println!(
                 "{}",
-                format!(
-                    "{:#06x}-{:#04x}-{:#04x}",
-                    splitted_date[2].parse::<u32>().unwrap_or_else(|_| {
-                        println!("Error while parsing year");
-                        process::exit(1_i32);
-                    }),
-                    splitted_date[1].parse::<u32>().unwrap_or_else(|_| {
-                        println!("Error while parsing month");
-                        process::exit(1_i32);
-                    }),
-                    splitted_date[0].parse::<i32>().unwrap_or_else(|_| {
-                        println!("Error while parsing day");
-                        process::exit(1_i32);
-                    }),
-                )
+                format_date((year, month, day), matches.is_present("balanced-ternary"))
             );
             process::exit(0_i32);
         } else {
@@ -130,6 +134,7 @@ fn main() {
     let show_weekend_marker =
         !(matches.is_present("disable-all") || matches.is_present("no-weekend"));
     let show_title_font_effect = !matches.is_present("disable-all") && matches.is_present("effect");
+    let balanced_ternary_flag = matches.is_present("balanced-ternary");
 
     if let Some(year) = matches.value_of("year") {
         if let Some(month) = matches.value_of("month") {
@@ -154,6 +159,7 @@ fn main() {
                         show_day_marker,
                         show_weekend_marker,
                         show_title_font_effect,
+                        balanced_ternary_flag,
                     );
                 } else {
                     let i32_year = year.parse::<i32>().unwrap_or_else(|_| {
@@ -175,6 +181,7 @@ fn main() {
                         show_day_marker,
                         show_weekend_marker,
                         show_title_font_effect,
+                        balanced_ternary_flag,
                     );
                 }
             } else if [month, year].iter().all(|&elem| elem.starts_with("0x")) {
@@ -191,6 +198,7 @@ fn main() {
                     false,
                     show_weekend_marker,
                     show_title_font_effect,
+                    balanced_ternary_flag,
                 );
             } else {
                 let i32_year = year.parse::<i32>().unwrap_or_else(|_| {
@@ -208,6 +216,7 @@ fn main() {
                     false,
                     show_weekend_marker,
                     show_title_font_effect,
+                    balanced_ternary_flag,
                 );
             }
         } else {
@@ -222,11 +231,20 @@ fn main() {
             show_day_marker,
             show_weekend_marker,
             show_title_font_effect,
+            balanced_ternary_flag,
         );
     }
 }
 
-fn hcal(year: i32, month: u32, day: u32, show_day: bool, show_weekend: bool, effect: bool) {
+fn hcal(
+    year: i32,
+    month: u32,
+    day: u32,
+    show_day: bool,
+    show_weekend: bool,
+    effect: bool,
+    balanced_ternary: bool,
+) {
     let now = NaiveDate::from_ymd(year, month, day);
     let year = now.year();
     let month = now.month();
@@ -234,17 +252,32 @@ fn hcal(year: i32, month: u32, day: u32, show_day: bool, show_weekend: bool, eff
     let days_from_monday = NaiveDate::from_ymd(year as i32, month, 1_u32)
         .weekday()
         .num_days_from_monday();
-    println!(
-        "\t\t\t{}\n\t\t\t  {}\n",
-        format!("{:#06x}", year),
-        format!("{:02x}", month)
-    );
+
+    if balanced_ternary {
+        println!(
+            "\t\t\t{}\n\t\t\t  {}\n",
+            int_to_bal_ternary(year as i128),
+            int_to_bal_ternary(month as i128)
+        );
+    } else {
+        println!(
+            "\t\t\t{}\n\t\t\t  {}\n",
+            format!("{:#06x}", year),
+            format!("{:02x}", month)
+        );
+    }
 
     println!("{}", headline(effect));
     let mut end = 7_u32 - days_from_monday;
     let mut vec = Vec::new();
     for x in 1_u32..=end {
-        vec.push(mark((year as i32, month, x), day, show_day, show_weekend));
+        vec.push(mark(
+            (year as i32, month, x),
+            day,
+            show_day,
+            show_weekend,
+            balanced_ternary,
+        ));
     }
     println!(
         "{}{}",
@@ -254,38 +287,87 @@ fn hcal(year: i32, month: u32, day: u32, show_day: bool, show_weekend: bool, eff
     end += 1_u32;
     vec = Vec::new();
     for x in end..end + 7_u32 {
-        vec.push(mark((year as i32, month, x), day, show_day, show_weekend));
+        vec.push(mark(
+            (year as i32, month, x),
+            day,
+            show_day,
+            show_weekend,
+            balanced_ternary,
+        ));
     }
     println!("{}", vec.join("\t"));
     end += 7_u32;
     vec = Vec::new();
     for x in end..end + 7_u32 {
-        vec.push(mark((year as i32, month, x), day, show_day, show_weekend));
+        vec.push(mark(
+            (year as i32, month, x),
+            day,
+            show_day,
+            show_weekend,
+            balanced_ternary,
+        ));
     }
     println!("{}", vec.join("\t"));
     end += 7_u32;
     vec = Vec::new();
     for x in end..end + 7_u32 {
-        vec.push(mark((year as i32, month, x), day, show_day, show_weekend));
+        vec.push(mark(
+            (year as i32, month, x),
+            day,
+            show_day,
+            show_weekend,
+            balanced_ternary,
+        ));
     }
     println!("{}", vec.join("\t"));
     end += 7_u32;
     vec = Vec::new();
     if cal::get_days_from_month(year as i32, month as u32) as u32 - end < 7_u32 {
         for x in end..=cal::get_days_from_month(year as i32, month as u32) as u32 {
-            vec.push(mark((year as i32, month, x), day, show_day, show_weekend));
+            vec.push(mark(
+                (year as i32, month, x),
+                day,
+                show_day,
+                show_weekend,
+                balanced_ternary,
+            ));
         }
         println!("{}", vec.join("\t"));
     } else {
         for x in end..=end + 6_u32 {
-            vec.push(mark((year as i32, month, x), day, show_day, show_weekend));
+            vec.push(mark(
+                (year as i32, month, x),
+                day,
+                show_day,
+                show_weekend,
+                balanced_ternary,
+            ));
         }
         println!("{}", vec.join("\t"));
         vec = Vec::new();
         for x in end + 7_u32..=cal::get_days_from_month(year as i32, month as u32) as u32 {
-            vec.push(mark((year as i32, month, x), day, show_day, show_weekend));
+            vec.push(mark(
+                (year as i32, month, x),
+                day,
+                show_day,
+                show_weekend,
+                balanced_ternary,
+            ));
         }
         println!("{}", vec.join("\t"));
+    }
+}
+
+fn format_date(date: (i32, u32, u32), balanced_ternary: bool) -> std::string::String {
+    if balanced_ternary {
+        return format!(
+            "{}:{}:{}",
+            int_to_bal_ternary(date.0 as i128),
+            int_to_bal_ternary(date.1 as i128),
+            int_to_bal_ternary(date.2 as i128)
+        );
+    } else {
+        return format!("{:#06x}-{:#04x}-{:#04x}", date.0, date.1, date.2);
     }
 }
 
@@ -302,15 +384,34 @@ fn mark(
     day: u32,
     show_day: bool,
     show_weekend: bool,
+    balanced_ternary: bool,
 ) -> std::string::String {
     use chrono::Weekday;
     let the_day = NaiveDate::from_ymd(date_tuple.0, date_tuple.1, date_tuple.2);
     let weekday = the_day.weekday();
     if date_tuple.2 == day && show_day {
-        return format!("\u{001b}[41m{:#04x}\u{001b}[0m", date_tuple.2);
+        if balanced_ternary {
+            return format!(
+                "\u{001b}[41m{}\u{001b}[0m",
+                int_to_bal_ternary(date_tuple.2 as i128)
+            );
+        } else {
+            return format!("\u{001b}[41m{:#04x}\u{001b}[0m", date_tuple.2);
+        }
     } else if [Weekday::Sat, Weekday::Sun].contains(&weekday) && show_weekend {
-        return format!("\u{001b}[7m{:#04x}\u{001b}[0m", date_tuple.2);
+        if balanced_ternary {
+            return format!(
+                "\u{001b}[7m{}\u{001b}[0m",
+                int_to_bal_ternary(date_tuple.2 as i128)
+            );
+        } else {
+            return format!("\u{001b}[7m{:#04x}\u{001b}[0m", date_tuple.2);
+        }
     } else {
-        return format!("{:#04x}", date_tuple.2);
+        if balanced_ternary {
+            return format!("{}", int_to_bal_ternary(date_tuple.2 as i128));
+        } else {
+            return format!("{:#04x}", date_tuple.2);
+        }
     }
 }
