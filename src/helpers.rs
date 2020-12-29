86a05219a4ddeddf654ae1244d73ad165e950a48
0xflotus <0xflotus@gmail.com>
2020-12-29 15:45:01 +0100

    formatting

--- src/helpers.rs
+++ src/helpers.rs
@@ -2,7 +2,10 @@ pub mod cal {
     use chrono::NaiveDate;
 
     // from https://stackoverflow.com/a/58188385
-    pub fn get_days_from_month(year: i32, month: u32) -> i64 {
+    pub fn get_days_from_month(
+        year: i32,
+        month: u32,
+    ) -> i64 {
         NaiveDate::from_ymd(
             match month {
                 0xc_u32 => year + 1_i32,
@@ -14,7 +17,9 @@ pub mod cal {
             },
             1_u32,
         )
-        .signed_duration_since(NaiveDate::from_ymd(year, month, 1_u32))
+        .signed_duration_since(NaiveDate::from_ymd(
+            year, month, 1_u32,
+        ))
         .num_days()
     }
 }
@@ -24,14 +29,22 @@ pub mod hex {
 
     const PREFIX: &str = "0x";
 
-    pub fn trim_and_parse_hex(hex: &str) -> Result<i64, num::ParseIntError> {
-        return i64::from_str_radix(hex.trim_start_matches(PREFIX), 0x10_u32);
+    pub fn trim_and_parse_hex(
+        hex: &str,
+    ) -> Result<i64, num::ParseIntError> {
+        return i64::from_str_radix(
+            hex.trim_start_matches(PREFIX),
+            0x10_u32,
+        );
     }
 }
 
 pub mod fmt {
     use cbb::util::cbb::int_to_bal_ternary;
-    pub fn format_date(date: (i32, u32, u32), balanced_ternary: bool) -> std::string::String {
+    pub fn format_date(
+        date: (i32, u32, u32),
+        balanced_ternary: bool,
+    ) -> std::string::String {
         if balanced_ternary {
             return format!(
                 "{}:{}:{}",
@@ -40,7 +53,10 @@ pub mod fmt {
                 int_to_bal_ternary(date.2 as i128)
             );
         } else {
-            return format!("{:#06x}-{:#04x}-{:#04x}", date.0, date.1, date.2);
+            return format!(
+                "{:#06x}-{:#04x}-{:#04x}",
+                date.0, date.1, date.2
+            );
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@ mod helpers;
 use helpers::{cal, fmt, hex};
 
 fn main() {
-    const VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
+    const VERSION: Option<&'static str> =
+        option_env!("CARGO_PKG_VERSION");
     let matches = App::new("hcal")
         .version(VERSION.unwrap_or("n/a"))
         .about("A hexadecimal calendar")
@@ -82,10 +83,12 @@ fn main() {
         .get_matches();
 
     if let Some(easter) = matches.value_of("easter") {
-        let easter_date = easter::easter_naive_date(easter.parse::<i32>().unwrap_or_else(|_| {
-            println!("Error while parsing year");
-            process::exit(1_i32);
-        }) as i32)
+        let easter_date = easter::easter_naive_date(
+            easter.parse::<i32>().unwrap_or_else(|_| {
+                println!("Error while parsing year");
+                process::exit(1_i32);
+            }) as i32,
+        )
         .unwrap_or_else(|error| {
             println!("{}", error);
             process::exit(1_i32);
@@ -93,7 +96,11 @@ fn main() {
         println!(
             "{}",
             fmt::format_date(
-                (easter_date.year(), easter_date.month(), easter_date.day()),
+                (
+                    easter_date.year(),
+                    easter_date.month(),
+                    easter_date.day()
+                ),
                 matches.is_present("balanced-ternary")
             )
         );
@@ -101,27 +108,38 @@ fn main() {
     }
 
     if let Some(date) = matches.value_of("transform") {
-        let re = Regex::new(r"^\d{2}-\d{2}-\d{4}$").unwrap_or_else(|_| {
-            println!("Error while parsing date");
-            process::exit(1_i32);
-        });
+        let re = Regex::new(r"^\d{2}-\d{2}-\d{4}$")
+            .unwrap_or_else(|_| {
+                println!("Error while parsing date");
+                process::exit(1_i32);
+            });
         if re.is_match(date) {
-            let splitted_date: Vec<&str> = date.split('-').collect();
-            let year = splitted_date[2].parse::<i32>().unwrap_or_else(|_| {
-                println!("Error while parsing year");
-                process::exit(1_i32);
-            });
-            let month = splitted_date[1].parse::<u32>().unwrap_or_else(|_| {
-                println!("Error while parsing month");
-                process::exit(1_i32);
-            });
-            let day = splitted_date[0].parse::<u32>().unwrap_or_else(|_| {
-                println!("Error while parsing day");
-                process::exit(1_i32);
-            });
+            let splitted_date: Vec<&str> =
+                date.split('-').collect();
+            let year = splitted_date[2]
+                .parse::<i32>()
+                .unwrap_or_else(|_| {
+                    println!("Error while parsing year");
+                    process::exit(1_i32);
+                });
+            let month = splitted_date[1]
+                .parse::<u32>()
+                .unwrap_or_else(|_| {
+                    println!("Error while parsing month");
+                    process::exit(1_i32);
+                });
+            let day = splitted_date[0]
+                .parse::<u32>()
+                .unwrap_or_else(|_| {
+                    println!("Error while parsing day");
+                    process::exit(1_i32);
+                });
             println!(
                 "{}",
-                fmt::format_date((year, month, day), matches.is_present("balanced-ternary"))
+                fmt::format_date(
+                    (year, month, day),
+                    matches.is_present("balanced-ternary")
+                )
             );
             process::exit(0_i32);
         } else {
@@ -130,11 +148,17 @@ fn main() {
         }
     }
 
-    let show_day_marker = !(matches.is_present("disable-all") || matches.is_present("disable"));
-    let show_weekend_marker =
-        !(matches.is_present("disable-all") || matches.is_present("no-weekend"));
-    let show_title_font_effect = !matches.is_present("disable-all") && matches.is_present("effect");
-    let balanced_ternary_flag = matches.is_present("balanced-ternary");
+    let show_day_marker = !(matches
+        .is_present("disable-all")
+        || matches.is_present("disable"));
+    let show_weekend_marker = !(matches
+        .is_present("disable-all")
+        || matches.is_present("no-weekend"));
+    let show_title_font_effect = !matches
+        .is_present("disable-all")
+        && matches.is_present("effect");
+    let balanced_ternary_flag =
+        matches.is_present("balanced-ternary");
 
     if let Some(year) = matches.value_of("year") {
         if let Some(month) = matches.value_of("month") {
@@ -162,18 +186,33 @@ fn main() {
                         balanced_ternary_flag,
                     );
                 } else {
-                    let i32_year = year.parse::<i32>().unwrap_or_else(|_| {
-                        println!("Error while parsing year");
-                        process::exit(1_i32);
-                    }) as i32;
-                    let u32_month = month.parse::<u32>().unwrap_or_else(|_| {
-                        println!("Error while parsing month");
-                        process::exit(1_i32);
-                    }) as u32;
-                    let u32_day = day.parse::<u32>().unwrap_or_else(|_| {
-                        println!("Error while parsing day");
-                        process::exit(1_i32);
-                    }) as u32;
+                    let i32_year = year
+                        .parse::<i32>()
+                        .unwrap_or_else(|_| {
+                            println!(
+                                "Error while parsing year"
+                            );
+                            process::exit(1_i32);
+                        })
+                        as i32;
+                    let u32_month = month
+                        .parse::<u32>()
+                        .unwrap_or_else(|_| {
+                            println!(
+                                "Error while parsing month"
+                            );
+                            process::exit(1_i32);
+                        })
+                        as u32;
+                    let u32_day = day
+                        .parse::<u32>()
+                        .unwrap_or_else(|_| {
+                            println!(
+                                "Error while parsing day"
+                            );
+                            process::exit(1_i32);
+                        })
+                        as u32;
                     hcal(
                         i32_year,
                         u32_month,
@@ -184,16 +223,25 @@ fn main() {
                         balanced_ternary_flag,
                     );
                 }
-            } else if [month, year].iter().all(|&elem| elem.starts_with("0x")) {
+            } else if [month, year]
+                .iter()
+                .all(|&elem| elem.starts_with("0x"))
+            {
                 hcal(
-                    hex::trim_and_parse_hex(year).unwrap_or_else(|_| {
-                        println!("Error while parsing year");
-                        process::exit(1_i32);
-                    }) as i32,
-                    hex::trim_and_parse_hex(month).unwrap_or_else(|_| {
-                        println!("Error while parsing month");
-                        process::exit(1_i32);
-                    }) as u32,
+                    hex::trim_and_parse_hex(year)
+                        .unwrap_or_else(|_| {
+                            println!(
+                                "Error while parsing year"
+                            );
+                            process::exit(1_i32);
+                        }) as i32,
+                    hex::trim_and_parse_hex(month)
+                        .unwrap_or_else(|_| {
+                            println!(
+                                "Error while parsing month"
+                            );
+                            process::exit(1_i32);
+                        }) as u32,
                     1_u32,
                     false,
                     show_weekend_marker,
@@ -201,14 +249,24 @@ fn main() {
                     balanced_ternary_flag,
                 );
             } else {
-                let i32_year = year.parse::<i32>().unwrap_or_else(|_| {
-                    println!("Error while parsing year");
-                    process::exit(1_i32);
-                }) as i32;
-                let u32_month = month.parse::<u32>().unwrap_or_else(|_| {
-                    println!("Error while parsing month");
-                    process::exit(1_i32);
-                }) as u32;
+                let i32_year = year
+                    .parse::<i32>()
+                    .unwrap_or_else(|_| {
+                        println!(
+                            "Error while parsing year"
+                        );
+                        process::exit(1_i32);
+                    })
+                    as i32;
+                let u32_month = month
+                    .parse::<u32>()
+                    .unwrap_or_else(|_| {
+                        println!(
+                            "Error while parsing month"
+                        );
+                        process::exit(1_i32);
+                    })
+                    as u32;
                 hcal(
                     i32_year,
                     u32_month,
@@ -249,9 +307,10 @@ fn hcal(
     let year = now.year();
     let month = now.month();
     let day = now.day();
-    let days_from_monday = NaiveDate::from_ymd(year as i32, month, 1_u32)
-        .weekday()
-        .num_days_from_monday();
+    let days_from_monday =
+        NaiveDate::from_ymd(year as i32, month, 1_u32)
+            .weekday()
+            .num_days_from_monday();
 
     if balanced_ternary {
         println!(
@@ -322,8 +381,16 @@ fn hcal(
     println!("{}", vec.join("\t"));
     end += 7_u32;
     vec = Vec::new();
-    if cal::get_days_from_month(year as i32, month as u32) as u32 - end < 7_u32 {
-        for x in end..=cal::get_days_from_month(year as i32, month as u32) as u32 {
+    if cal::get_days_from_month(year as i32, month as u32)
+        as u32
+        - end
+        < 7_u32
+    {
+        for x in end..=cal::get_days_from_month(
+            year as i32,
+            month as u32,
+        ) as u32
+        {
             vec.push(mark(
                 (year as i32, month, x),
                 day,
@@ -345,7 +412,12 @@ fn hcal(
         }
         println!("{}", vec.join("\t"));
         vec = Vec::new();
-        for x in end + 7_u32..=cal::get_days_from_month(year as i32, month as u32) as u32 {
+        for x in end + 7_u32
+            ..=cal::get_days_from_month(
+                year as i32,
+                month as u32,
+            ) as u32
+        {
             vec.push(mark(
                 (year as i32, month, x),
                 day,
@@ -374,7 +446,11 @@ fn mark(
     balanced_ternary: bool,
 ) -> std::string::String {
     use chrono::Weekday;
-    let the_day = NaiveDate::from_ymd(date_tuple.0, date_tuple.1, date_tuple.2);
+    let the_day = NaiveDate::from_ymd(
+        date_tuple.0,
+        date_tuple.1,
+        date_tuple.2,
+    );
     let weekday = the_day.weekday();
     if date_tuple.2 == day && show_day {
         if balanced_ternary {
@@ -383,20 +459,32 @@ fn mark(
                 int_to_bal_ternary(date_tuple.2 as i128)
             );
         } else {
-            return format!("\u{001b}[41m{:#04x}\u{001b}[0m", date_tuple.2);
+            return format!(
+                "\u{001b}[41m{:#04x}\u{001b}[0m",
+                date_tuple.2
+            );
         }
-    } else if [Weekday::Sat, Weekday::Sun].contains(&weekday) && show_weekend {
+    } else if [Weekday::Sat, Weekday::Sun]
+        .contains(&weekday)
+        && show_weekend
+    {
         if balanced_ternary {
             return format!(
                 "\u{001b}[7m{}\u{001b}[0m",
                 int_to_bal_ternary(date_tuple.2 as i128)
             );
         } else {
-            return format!("\u{001b}[7m{:#04x}\u{001b}[0m", date_tuple.2);
+            return format!(
+                "\u{001b}[7m{:#04x}\u{001b}[0m",
+                date_tuple.2
+            );
         }
     } else {
         if balanced_ternary {
-            return format!("{}", int_to_bal_ternary(date_tuple.2 as i128));
+            return format!(
+                "{}",
+                int_to_bal_ternary(date_tuple.2 as i128)
+            );
         } else {
             return format!("{:#04x}", date_tuple.2);
         }
